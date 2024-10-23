57fa44c013ff084be2148f9f4bf23115a3f360da
Lilianne Maia <lilianneps@gmail.com>
2024-10-22 22:16:12 -0300

    banner

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,9 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('banner-title').textContent = data.title;
-            document.getElementById('banner-description').textContent = data.description;
+let divBanner = document.getElementById("banner")
+let divTxt = document.getElementById("txt")
+
+fetch("settings.json").then((response) => {
+    response.json().then((dados) => {
+    let index = 0;
+    console.log(dados.banners.length)
+    
+    const trocarImg= () => {
+ 
+        index = (index + 1) % dados.banners.length;
+        divTxt.innerHTML = `<p id="txtBanner"> "${dados.banners[index].descricao}" </p>`
+        divBanner.innerHTML = `<img id="imgBanner" src="${dados.banners[index].imagem}" alt="600px" height="600px">`
+        
+    }
+        setInterval(trocarImg, 3000);
         })
-        .catch(error => console.error('Erro ao carregar os dados do banner:', error));
-});
+    })
