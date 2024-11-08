e8fa5f1a58b63b08176af989688283569959edf2
Lilianne Maia <lilianneps@gmail.com>
2024-11-08 09:55:36 -0300

    Delete .vscode directory

--- .vscode/assets/css/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('banner-title').textContent = data.title;
-            document.getElementById('banner-description').textContent = data.description;
-        })
-        .catch(error => console.error('Erro ao carregar os dados do banner:', error));
-});
